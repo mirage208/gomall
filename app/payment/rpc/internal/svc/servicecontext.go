7e4d1f88652840917153213726fd77b804939bf8
mirage208 <qilianshuo@qq.com>
2025-07-27 23:24:52 +0800

    feat(payment): update configuration and service context for MySQL and Redis integration

--- app/payment/rpc/internal/svc/servicecontext.go
+++ app/payment/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,29 @@
 package svc
 
-import "github.com/mirage208/gomall/app/payment/rpc/internal/config"
+import (
+	"github.com/mirage208/gomall/app/order/rpc/orderclient"
+	"github.com/mirage208/gomall/app/payment/model"
+	"github.com/mirage208/gomall/app/payment/rpc/internal/config"
+	"github.com/mirage208/gomall/app/user/rpc/userclient"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type ServiceContext struct {
 	Config config.Config
+
+	PaymentModel model.PayModel
+
+	UserRpc  userclient.User
+	OrderRpc orderclient.Order
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.Mysql.DataSource)
 	return &ServiceContext{
-		Config: c,
+		Config:       c,
+		PaymentModel: model.NewPayModel(conn, c.CacheRedis),
+		UserRpc:      userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
+		OrderRpc:     orderclient.NewOrder(zrpc.MustNewClient(c.OrderRpc)),
 	}
 }
