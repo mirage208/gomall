e7340d4dc249ad971da4c142d7e4646b0f3094d7
mirage208 <qilianshuo@qq.com>
2025-07-27 19:16:25 +0800

    feat: implement user RPC service with login, register, and user info functionalities
    - Added user.yaml configuration for RPC service.
    - Created config structure for RPC server configuration.
    - Implemented LoginLogic, RegisterLogic, and UserInfoLogic for handling respective requests.
    - Developed UserServer to handle RPC calls for login, registration, and user information retrieval.
    - Established ServiceContext for managing service dependencies.
    - Generated protobuf files for user service with necessary request and response structures.
    - Implemented user client for interacting with the user RPC service.
    - Set up main entry point for starting the RPC server with reflection support in development mode.

---
+++ app/user/rpc/internal/logic/registerlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/mirage208/gomall/app/user/rpc/internal/svc"
+	"github.com/mirage208/gomall/app/user/rpc/pb/user"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type RegisterLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *RegisterLogic {
+	return &RegisterLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *RegisterLogic) Register(in *user.RegisterRequest) (*user.RegisterResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &user.RegisterResponse{}, nil
+}
