39597ea1cea985d84caca0d2ea40ff1f6be66c4c
mirage208 <qilianshuo@qq.com>
2025-07-27 23:46:18 +0800

    feat(cart): add MySQL and Redis configuration to service context and YAML file

--- app/cart/rpc/internal/svc/servicecontext.go
+++ app/cart/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,26 @@
 package svc
 
-import "github.com/mirage208/gomall/app/cart/rpc/internal/config"
+import (
+	"github.com/mirage208/gomall/app/cart/model"
+	"github.com/mirage208/gomall/app/cart/rpc/internal/config"
+	"github.com/mirage208/gomall/app/user/rpc/userclient"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type ServiceContext struct {
 	Config config.Config
+
+	CartModel model.CartModel
+
+	UserRpcClient userclient.User
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.Mysql.DataSource)
 	return &ServiceContext{
-		Config: c,
+		Config:        c,
+		CartModel:     model.NewCartModel(conn, c.CacheRedis),
+		UserRpcClient: userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
 	}
 }
