231303c326aaaeb12fb2902572b2b7aa8bc97bbc
mirage208 <qilianshuo@qq.com>
2025-07-28 22:58:49 +0800

    feat(build): enhance build scripts to include API image handling and fix RPC image naming

--- build/push.sh
+++ build/push.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
 for app in user payment product order cart; do
+
+    # Push API image
+    docker push crpi-uuzhidt8pbh6o5xf.cn-shanghai.personal.cr.aliyuncs.com/mirage208/mall:${app}-api
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to push ${app}-api image"
+        exit 1
+    fi
+
     # Push RPC image
-    docker push crpi-uuzhidt8pbh6o5xf.cn-shanghai.personal.cr.aliyuncs.com/mirage208/mall:${app}_rpc
+    docker push crpi-uuzhidt8pbh6o5xf.cn-shanghai.personal.cr.aliyuncs.com/mirage208/mall:${app}-rpc
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to push ${app}-rpc image"
+        exit 1
+    fi
+
 done
