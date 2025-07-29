f9909b0e067f5d3b6872723c397a329bd34c3b2f
mirage208 <qilianshuo@qq.com>
2025-07-29 23:59:24 +0800

    feat: add CreateRevert logic with corresponding RPC methods and protobuf definitions

---
+++ app/order/rpc/internal/logic/createrevertlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/mirage208/gomall/app/order/rpc/internal/svc"
+	"github.com/mirage208/gomall/app/order/rpc/pb/order"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type CreateRevertLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewCreateRevertLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CreateRevertLogic {
+	return &CreateRevertLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *CreateRevertLogic) CreateRevert(in *order.CreateRequest) (*order.CreateResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &order.CreateResponse{}, nil
+}
